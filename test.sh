19ed15a7ffa005ea4f28683e79a60986bae293a0
merwanehamadi <merwanehamadi@gmail.com>
2023-08-23 19:41:11 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Check if a URL was provided as the first argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <url>"
-  echo "Please provide the URL as the first argument."
+# Check if a URL was provided as an environment variable
+if [ -z "$URL" ]; then
+  echo "Usage: URL=<url> bash <script>"
+  echo "Please provide the URL as an environment variable."
   exit 1
 fi
 
@@ -20,9 +20,6 @@ if ! command -v newman &> /dev/null; then
 else
   echo "newman is already installed"
 fi
-
-# Use the provided URL as the value for the env-var
-URL="$1"
 
 newman run https://raw.githubusercontent.com/Significant-Gravitas/postman/master/Postman%20Collections/agent_protocol_v0.4.json \
 -e https://raw.githubusercontent.com/Significant-Gravitas/postman/master/Postman%20Collections/env_0.4.json \
